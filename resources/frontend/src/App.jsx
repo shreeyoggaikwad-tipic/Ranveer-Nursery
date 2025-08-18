b00241f5dbeb2f8ac666958c994c33ad776f286f
Shreeyog Gaikwad <shreeyoggaikwad22@gmail.com>
2025-08-18 15:13:41 +0530

    Initial Setup Done

---
+++ resources/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl font-bold'>Welcome to the Civil Engineering Startup Website</h1>
+    </>
+  )
+}
+
+export default App
