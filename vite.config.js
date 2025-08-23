23309441938e6b0d47d4c7fe542419ae1dd32b21
shreeyoggaikwad-tipic <shreeyoggaikwad22@gmail.com>
2025-08-23 13:20:56 +0530

    Build Path Changed for frontend

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/frontend/src/index.css', 'resources/frontend/src/main.jsx'],
             refresh: true,
         }),
         tailwindcss(),
