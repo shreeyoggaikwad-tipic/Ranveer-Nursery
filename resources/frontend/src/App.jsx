ffa6e0e632c9543b1c54f70381727340cb8d68e8
shreeyoggaikwad-tipic <shreeyoggaikwad22@gmail.com>
2025-08-31 13:07:44 +0530

    Code Base cleanup

--- resources/frontend/src/App.jsx
+++ resources/frontend/src/App.jsx
@@ -11,37 +11,19 @@ import ServicesPage from './pages/ServicesPage';
 import ContactPage from './pages/ContactPage';
 import ReviewsPage from "./pages/ReviewsPage"
 
-
-import ProjectsPage from './pages/ProjectsPage'
-import TestimonialsPage from './pages/TestimonialsPage';
-import ProjectDetailsPage from './pages/ProjectDetailsPage';
-import ServiceDetailsPage from './pages/ServiceDetailsPage';
-
 // Admin pages
 import AdminLogin from "./pages/Admin/AdminLogin";
 import AdminProfile from './pages/Admin/AdminProfile';
 import ForgotPasswordPage from './pages/Admin/ForgotPasswordPage';
 import ResetPasswordPage from './pages/Admin/ResetPasswordPage';
-
-import AdminDashboard from './pages/Admin/AdminDashboard';
-import ManageProjects from './pages/Admin/ManageProjects';
-import ManageServices from './pages/Admin/ManageServices';
-import ManageTestimonials from './pages/Admin/ManageTestimonials';
 import ManageInquiries from './pages/Admin/ManageInquiries';
-import AddProjectForm from './pages/Admin/AddProjectForm';
-import AddServiceForm from './pages/Admin/AddServiceForm';
-import AddTestimonialForm from './pages/Admin/AddTestimonialForm';
-import EditServiceForm from './pages/Admin/EditServiceForm';
-import EditProjectForm from './pages/Admin/EditProjectForm';
-import EditTestimonialForm from './pages/Admin/EditTestimonialForm';
+
 
 // 🔒 Simple Private Route Wrapper
 function PrivateRoute({ children }) {
   const token = localStorage.getItem("token"); // store Sanctum token after login
   return token ? children : <Navigate to="/admin/login" />;
 }
-
-
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -72,45 +54,15 @@ function App() {
         <Route path="/plants" element={<PlantsPage />} />
         <Route path="/services" element={<ServicesPage />} />
         {/* <Route path="/reviews" element={<ReviewsPage />} /> */}
+        {/* <Route path="/tools" element={<ToolsPage />} /> */}
         <Route path="/contact" element={<ContactPage />} />
 
         {/* Admin routes */}
         <Route path="/admin/login" element={<AdminLogin />} />
         <Route path="/admin/forgot-password" element={<ForgotPasswordPage />} />
         <Route path="/admin/reset-password" element={<ResetPasswordPage />} />
-
-        <Route path="/admin/dashboard" element={
-          <PrivateRoute><AdminDashboard /></PrivateRoute>
-        } />
-        <Route path="/admin/projects" element={
-          <PrivateRoute><ManageProjects /></PrivateRoute>
-        } />
-        <Route path="/admin/services" element={
-          <PrivateRoute><ManageServices /></PrivateRoute>
-        } />
-        <Route path="/admin/testimonials" element={
-          <PrivateRoute><ManageTestimonials /></PrivateRoute>
-        } />
         <Route path="/admin/inquiries" element={
           <PrivateRoute><ManageInquiries /></PrivateRoute>
-        } />
-        <Route path="/admin/addProject" element={
-          <PrivateRoute><AddProjectForm /></PrivateRoute>
-        } />
-        <Route path="/admin/addService" element={
-          <PrivateRoute><AddServiceForm /></PrivateRoute>
-        } />
-        <Route path="/admin/addTestimonial" element={
-          <PrivateRoute><AddTestimonialForm /></PrivateRoute>
-        } />
-        <Route path="/admin/editService/:id" element={
-          <PrivateRoute><EditServiceForm /></PrivateRoute>
-        } />
-        <Route path="/admin/editProject/:id" element={
-          <PrivateRoute><EditProjectForm /></PrivateRoute>
-        } />
-        <Route path="/admin/editTestimonial/:id" element={
-          <PrivateRoute><EditTestimonialForm /></PrivateRoute>
         } />
         <Route path="/admin/profile" element={
           <PrivateRoute><AdminProfile /></PrivateRoute>
--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\InquiryController;
-use App\Http\Controllers\TestimonialController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 
@@ -15,14 +13,6 @@ Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware('auth
 Route::post('/auth/forgot-password', [AuthController::class, 'forgotPassword']);
 Route::post('/auth/reset-password', [AuthController::class, 'resetPassword']);
 
-// Public routes (accessible without authentication)
-Route::get('/projects', [ProjectController::class, 'index']);
-Route::get('/projects/{id}', [ProjectController::class, 'show']);
-Route::get('/services', [ServiceController::class, 'index']);
-Route::get('/services/{id}', [ServiceController::class, 'show']);
-Route::get('/testimonials', [TestimonialController::class, 'index']);
-Route::get('/testimonials/{id}', [TestimonialController::class, 'show']);
-
 // Contact/Inquiry submission (public)
 Route::post('/inquiries', [InquiryController::class, 'store']);
 
@@ -30,33 +20,13 @@ Route::get('/users/{id}', [UserController::class, 'show']);
 
 // Protected Admin routes
 Route::middleware(['auth:sanctum', 'is_admin'])->group(function () {
-    // Projects management (Admin only)
-    Route::post('/projects', [ProjectController::class, 'store']);
-    Route::put('/projects/{id}', [ProjectController::class, 'update']);
-    Route::delete('/projects/{id}', [ProjectController::class, 'destroy']);
-    
-    // Services management (Admin only)
-    Route::post('/services', [ServiceController::class, 'store']);
-    Route::put('/services/{id}', [ServiceController::class, 'update']);
-    Route::delete('/services/{id}', [ServiceController::class, 'destroy']);
-    
-    // Testimonials management (Admin only)
-    Route::post('/testimonials', [TestimonialController::class, 'store']);
-    Route::put('/testimonials/{id}', [TestimonialController::class, 'update']);
-    Route::delete('/testimonials/{id}', [TestimonialController::class, 'destroy']);
     
     // View inquiries (Admin only)
     Route::get('/inquiries', [InquiryController::class, 'index']);
     Route::get('/inquiries/export', [InquiryController::class, 'export']);
-    // Route::get('/inquiries/{id}', [InquiryController::class, 'show']); // To be Checked
     Route::delete('/inquiries/{id}', [InquiryController::class, 'destroy']);
     Route::patch('/inquiries/{id}/toggle-served', [InquiryController::class, 'toggleRequestServed']);
 
-    
-    // Admin user info
-    // Route::get('/user', function (Request $request) {
-    //     return $request->user();
-    // });
     Route::get('/user', [AuthController::class, 'user']);
     Route::get('/profile', [AuthController::class, 'getAdminProfile']);
     Route::put('/profile', [AuthController::class, 'updateAdminProfile']);
