cfadc5fd9225edf089a168b9c46ba57d8a165f91
shreeyoggaikwad-tipic <shreeyoggaikwad22@gmail.com>
2025-09-18 17:37:07 +0530

    Status removed from Manage Inquiries

--- resources/frontend/src/pages/Admin/ManageInquiries.jsx
+++ resources/frontend/src/pages/Admin/ManageInquiries.jsx
@@ -99,6 +99,7 @@ function ManageInquiries() {
         headers: { Authorization: `Bearer ${token}` },
       });
 
+      setInquiries((prev) => prev.filter((inq) => inq.id !== id));
       setInquiries2((prev) => prev.filter((inq) => inq.id !== id));
     } catch (err) {
       console.error("Delete failed:", err);
@@ -198,7 +199,7 @@ function ManageInquiries() {
                       <th className="px-6 py-4 border-b">Email</th>
                       <th className="px-6 py-4 border-b">Phone</th>
                       <th className="px-6 py-4 border-b">Message</th>
-                      {selectedCompany=='amar' ? null : <th className="px-6 py-4 border-b text-center">Status</th>}
+                      {/* {selectedCompany=='amar' ? null : <th className="px-6 py-4 border-b text-center">Status</th>} */}
                       <th className="px-6 py-4 border-b text-center">Actions</th>
                     </tr>
                   </thead>
@@ -216,8 +217,7 @@ function ManageInquiries() {
                           </td>
                           <td className="px-6 py-4 border-b">{inq.phone}</td>
                           <td className="px-6 py-4 border-b">{inq.message}</td>
-                          {selectedCompany=='amar' ? null :<td className="px-6 py-4 border-b text-center">
-                            {/* Toggle Switch */}
+                          {/* {selectedCompany=='amar' ? null :<td className="px-6 py-4 border-b text-center">
                             <div className="flex items-center justify-center">
                               <button
                                 onClick={() => toggleRequestServed(inq.id)}
@@ -247,7 +247,7 @@ function ManageInquiries() {
                                 {inq.request_served ? "Served" : "Pending"}
                               </span>
                             </div>
-                          </td>}
+                          </td>} */}
                           {/* Delete Action */}
                           <td className="px-6 py-4 border-b text-center">
                             <button
