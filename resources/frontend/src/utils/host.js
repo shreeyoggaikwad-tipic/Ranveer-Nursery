8dfeb11acd15bbbc892f1fb7b90308bed558cc74
shreeyoggaikwad-tipic <shreeyoggaikwad22@gmail.com>
2025-10-16 15:33:19 +0530

    Final Changes done

--- resources/frontend/src/utils/host.js
+++ resources/frontend/src/utils/host.js
@@ -1,5 +1,5 @@
-//  const host = "http://localhost:8000";
+ const host = "http://127.0.0.1:8000";
 // const host = "https://ranveer.tipic.co.in";
-const host = "https://ranveerrosenursery.com";
+// const host = "https://ranveerrosenursery.com";
 
 export default host;
